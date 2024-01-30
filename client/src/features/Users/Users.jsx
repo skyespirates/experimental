d693062188086e30fb8edbf7fe23ed796777d0c4
Aziz <azizwali414@gmail.com>
2024-01-30 10:27:29 +0700

    add fetch user by id

--- client/src/features/Users/Users.jsx
+++ client/src/features/Users/Users.jsx
@@ -1,25 +1,80 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { fetchUsers } from "./userSlice";
+import { useEffect, useState } from "react";
+import { fetchUsers, fetchUserById } from "./userSlice";
 
 const Users = () => {
   const dispatch = useDispatch();
   const state = useSelector((state) => state.users);
 
+  const [userId, setUserId] = useState(null);
+
   useEffect(() => {
     dispatch(fetchUsers());
   }, []);
+
+  const handleFetchUserById = (userId) => {
+    dispatch(fetchUserById(userId));
+  };
+
   return (
     <div>
-      {state.status === "loading" && <p>Loading...</p>}
-      {state.status === "succeeded" && (
-        <ul>
-          {state.data.map((user) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      )}
-      {state.status === "failed" && <p>{state.error.message}</p>}
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="user id"
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
+          />
+          <button onClick={() => handleFetchUserById(userId)}>
+            search user
+          </button>
+        </div>
+        <div>
+          {state.userStatus === "loading" && <p>Loading...</p>}
+          {state.userStatus === "succeeded" && (
+            <table>
+              <tr>
+                <td>id</td>
+                <td>:</td>
+                <td>{state.selectedUser.id}</td>
+              </tr>
+              <tr>
+                <td>name</td>
+                <td>:</td>
+                <td>{state.selectedUser.name}</td>
+              </tr>
+              <tr>
+                <td>username</td>
+                <td>:</td>
+                <td>{state.selectedUser.username}</td>
+              </tr>
+              <tr>
+                <td>email</td>
+                <td>:</td>
+                <td>{state.selectedUser.email}</td>
+              </tr>
+              <tr>
+                <td>phone</td>
+                <td>:</td>
+                <td>{state.selectedUser.phone}</td>
+              </tr>
+            </table>
+          )}
+          {state.userStatus === "failed" && <p>{state.userError.message}</p>}
+        </div>
+      </div>
+      <div>
+        {state.status === "loading" && <p>Loading...</p>}
+        {state.status === "succeeded" && (
+          <ul>
+            {state.data.map((user) => (
+              <li key={user.id}>{user.name}</li>
+            ))}
+          </ul>
+        )}
+        {state.status === "failed" && <p>{state.error.message}</p>}
+      </div>
     </div>
   );
 };
--- client/src/features/Users/userSlice.js
+++ client/src/features/Users/userSlice.js
@@ -1,16 +1,29 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const fetchUsers = createAsyncThunk("users/fetchusers", async () => {
+export const fetchUsers = createAsyncThunk("user/fetchUsers", async () => {
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/users?_limit=5"
   );
   return data;
 });
 
+export const fetchUserById = createAsyncThunk(
+  "user/fetchUserById",
+  async (userId) => {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${userId}`
+    );
+    return response.data;
+  }
+);
+
 const userSlice = createSlice({
   name: "users",
   initialState: {
+    selectedUser: null,
+    userStatus: "idle",
+    userError: null,
     data: [],
     status: "idle",
     error: null,
@@ -28,6 +41,17 @@ const userSlice = createSlice({
       .addCase(fetchUsers.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error;
+      })
+      .addCase(fetchUserById.pending, (state) => {
+        state.userStatus = "loading";
+      })
+      .addCase(fetchUserById.fulfilled, (state, action) => {
+        state.userStatus = "succeeded";
+        state.selectedUser = action.payload;
+      })
+      .addCase(fetchUserById.rejected, (state, action) => {
+        state.userStatus = "failed";
+        state.userError = action.error;
       });
   },
 });
